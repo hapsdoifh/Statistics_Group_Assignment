86711d65e92adca6a5e7568c91bca3aaa3e1329c
linh-KAK <114237304+linh-KAK@users.noreply.github.com>
2022-10-06 18:41:42 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,5 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        double[] input_array = new double[10];
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Enter a number: ");
-            double num = input.nextDouble();
-            input_array[i] = num;
-        }
-
-        System.out.println("The elements in this array are: ");
-        for (double element: input_array) {
-            System.out.print(element + " ");
-        }
-
-        StandardDeviation SD = new StandardDeviation();
-        System.out.format("\n\nThe Standard Deviation is: %.6f", StandardDeviation.calculateSD(input_array));
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
     }
-
 }
