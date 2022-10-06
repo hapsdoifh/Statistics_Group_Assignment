d7d434a4bd8ee838237eac31dc45e0b365c2e14f
hapsdoifh <zuhanwang@outlook.com>
2022-10-06 11:25:43 -0400

    Added abstract

--- Statistic.java
+++ Statistic.java
@@ -1,2 +1,11 @@
+interface Stats{
+    public double max(double[] List);
+    public double min(double[] List);
+    public double Mean(double[] List);
+    public double Median(double[] List);
+    public double Mode(Double[] List);
+    public double Range(Double[] Mode);
+}
+
 public class Statistic {
 }
