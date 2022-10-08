14be871857f3eaa1b07bf10dfd757a6e1e3c0485
linh-KAK <114237304+linh-KAK@users.noreply.github.com>
2022-10-08 19:42:31 -0400

    Updated Statistic

--- Statistic.java
+++ Statistic.java
@@ -9,6 +9,8 @@ interface Stats{
 
 public class Statistic {
     private double[] statsArray;
+    private double mean;
+    private double sd;
 
     public void setArray(double[] inputArr){
         statsArray = inputArr;
@@ -16,4 +18,25 @@ public class Statistic {
     public double[] getArray(){
         return statsArray;
     }
+
+    public void setMean() {
+        medianMeanMode averages = new medianMeanMode();
+        mean = averages.getMean(statsArray);
+    }
+
+    public double getMean() {
+        return mean;
+    }
+
+    public void setSD() {
+        StandardDeviation sd = new StandardDeviation();
+        this.sd = sd.calculateSD(statsArray, mean);
+    }
+
+    public double getSD() {
+        return sd;
+    }
+
+
+
 }
