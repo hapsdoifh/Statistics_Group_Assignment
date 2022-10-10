24a4c45f01e3a6dde5c298f0a67f0434d8ee3dea
HarryWang <50002801+hapsdoifh@users.noreply.github.com>
2022-10-10 11:55:58 -0400

    Added basic Box and Whisker plot maker

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 import java.util.*;
+
 public class Main{
     public static void main(String[] args){
+        double[] temp = {10,12,15,30,35,36,38,60,65,76,90,120};
+        Statistic myStat = new Statistic();
+        myStat.setArray(temp);
+        myStat.Quartiles();
+        outputGUI.outputGraph(myStat);
     }
 }
